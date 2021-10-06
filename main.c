c80218cd15d7d00d7d02aa46f9f493b2736e355f
Marc <marc.espuna@students.salle.url.edu>
2021-10-06 09:42:24 +0200

    primer commit

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
+
 int main () {
 
-	printf("Hola alumn@s de Projectes!!:)\n");
-
+	printf("Hola alumn@s de Projectes!!:)\n");	
+	printf("Test introduit per alumnes\n");
+	
 	return 0;
 }
