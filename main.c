71895ea220eafd6cdf1ff9fabea76b7f33dbf3fe
alexferrelopez <92015364+alexferrelopez@users.noreply.github.com>
2021-10-07 11:41:23 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main () {
-	printf("Menú:\n	 1. Afegir repo\n	2. Modificar repo\n	3. Eliminar repo\n");
+	printf("Menú:\n	 1. Afegir repo\n	2. Modificar repo\n	3. Eliminar repo\n	4. Sortir\n");
 	return 0;
 }
