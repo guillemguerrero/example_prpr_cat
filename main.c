f2868c0cfe6ff86f08666b08eddce58adc9160de
Marc <marc.espuna@students.salle.url.edu>
2021-10-07 10:45:00 +0200

    creacio de struct

--- main.c
+++ main.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
+typedef struct UnStructGuay
+{
+	int nivellDeGuay;
+	void* algo;
+}UnStructGuay;
+
 
 int main () {
 
 	printf("Hola alumn@s de Projectes!!:)\n");	
 	printf("Test introduit per alumnes\n");
-	
+
+	int a = 30;
+
+	UnStructGuay structGuay = { 10000, &a} ;
+	printf("%d", structGuay.nivellDeGuay);
+
 	return 0;
 }
