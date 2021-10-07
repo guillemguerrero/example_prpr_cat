aa9e22676f45f0609e65cb3dc30bbd0147280129
Claudia Lajara <claudia@Claudias-MacBook-Pro.local>
2021-10-07 10:56:11 +0200

    Main: s'ha afegit un missatge

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
 int main () {
+        int i = 0;
 
 	printf("Hola alumn@s de Projectes!!:)\n");
+        
+        for (i = 0; i < 3; i++) {        
+             printf("El codi és: 861 3614 3855\n");
+        }
 
 	return 0;
 }
