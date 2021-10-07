b2b2b74ff9ce656aa19646b1c38540f104d43c25
TomDevUz <69120787+TomDevUz@users.noreply.github.com>
2021-10-07 10:52:06 +0200

    Mensaje de Prueba

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
 int main () {
+	int i, j;
 
 	printf("Hola alumn@s de Projectes!!:)\n");
+
+	for (i = 0; i < 10; i++) {
+	    for (j = 0; i < 10; j++) {
+		printf ("*");
+	    }
+	    printf ("\n");
+	}
 
 	return 0;
 }
