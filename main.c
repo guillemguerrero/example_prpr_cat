6d99e43f63bb689cf8a7db6491d0584d8f1b9f18
TomDevUz <69120787+TomDevUz@users.noreply.github.com>
2021-10-07 11:08:21 +0200

    Print Square Function

--- main.c
+++ main.c
@@ -1,13 +1,21 @@
+/*********************************
+ *  Created by Tomas Uzcudun
+ *  Date: 07/10/2021
+ *  Program: Print Square
+ ********************************/
+
 #include <stdio.h>
+
+#define SIZE 5
 
 int main () {
 	int i, j;
 
-	printf("Hola alumn@s de Projectes!!:)\n");
+	printf("A Square\n\n");
 
-	for (i = 0; i < 10; i++) {
-	    for (j = 0; i < 10; j++) {
-		printf ("*");
+	for (i = 0; i < SIZE; i++) {
+	    for (j = 0; j < SIZE; j++) {
+	        printf ("*");
 	    }
 	    printf ("\n");
 	}
