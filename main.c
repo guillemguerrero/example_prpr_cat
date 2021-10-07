8aa7625318d090ef67022b3872eec62c47b1db58
Thaedan <josem.vallssds@gmail.com>
2021-10-07 11:39:57 +0200

    3 prpr

--- main.c
+++ main.c
@@ -3,7 +3,8 @@
 int main () {
 
 	printf("Menu\n");
-	printf("Afegir repo: \n");
-	printf("Modificar repo: \n");
+	printf("1-Afegir repo: \n");
+	printf("2-Modificar repo: \n");
+	printf("3-Eliminar repo: \n");
 	return 0;
 }
