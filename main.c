72d839b05faf6f9b73825077bce269d7eb637387
Àlex Ferré <alex.fl@students.salle.url.edu>
2021-10-07 11:05:09 +0200

    Odio els punters

--- main.c
+++ main.c
@@ -2,7 +2,15 @@
 
 int main () {
 
+	int a = 0;
+	int* A,b;
+	A = &a;
+	B = B;
 	printf("Hola alumn@s de Projectes!\n");
+	printf("Com anaven els punters?\n");
+	printf("Valor del punter: %d\n, Adreça del punter: /d",*B, A);
+
+
 
 	return 0;
 }
