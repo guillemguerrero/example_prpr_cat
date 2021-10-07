38a3b5c49c1213ba260549fbf2ba814d3f41e9fb
jordirull <j.rullsegura@hotmail.com>
2021-10-07 10:43:03 +0200

    main.c

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
 int main () {
+	
+	int edat;
 
 	printf("Hola alumn@s de Projectes!!:)\n");
+	printf("Em dic Jordi Rull\n");
+	
+	printf("introdueix la teva edat: \n");
+	scanf("%d", &edat);
 
 	return 0;
 }
