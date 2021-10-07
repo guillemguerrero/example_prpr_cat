2424c2fd140b51aca1b004777ed9d045ab78e33c
Marc Valsells Niubo <marc@valsells.me>
2021-10-07 10:48:31 +0200

    Canviat el print de projectes per SO

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-
+#include <string.h>
+#include <unistd.h>
 int main () {
-
-	printf("Hola alumn@s de Projectes!!:)\n");
-
+	//printf("Hola alumn@s de Projectes!!:)\n");
+	write(1, "Hola alumn@s de SO!! :)\n", strlen("Hola alumn@s de SO!! :)\n"));
 	return 0;
 }
