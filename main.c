fe7d6ac35334a42bc5743fa3279cfec8e75bef3b
oriolGonzalez <oriol5000@gmail.com>
2021-10-07 11:40:57 +0200

    modificar repo

--- main.c
+++ main.c
@@ -5,10 +5,14 @@ int main () {
 
 	printf("Menu\n");
 	printf("1.- Afegir repo\n");
+	printf("2.- Modificar repo\n");
 	do{
 		switch(opcio){
 		case 1:
 			printf("Afegir repo");
+			break;
+		case 2:
+			printf("MOdificar repo");
 			break;
 		default:
 			printf("No has introducido un numero valido! :\(");
