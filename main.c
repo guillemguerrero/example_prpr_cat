6e7b41a4e34dc15d2271a09bb574ed02aec72cdd
Marc Valsells Niubo <marc@valsells.me>
2021-10-07 11:36:41 +0200

    commit 1 prpr

--- main.c
+++ main.c
@@ -4,5 +4,7 @@
 int main () {
 	//printf("Hola alumn@s de Projectes!!:)\n");
 	write(1, "Hola alumn@s de SO!! :)\n", strlen("Hola alumn@s de SO!! :)\n"));
+	printf("Menú:\n\t1.Afegir repo")
+
 	return 0;
 }
