3759f590a5a852eb846fd6e63dc84d427a3a74c6
oriolGonzalez <oriol5000@gmail.com>
2021-10-07 11:40:26 +0200

    afegir repo

--- main.c
+++ main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
 int main () {
+	int opcio = 0;
 
-	printf("Hola alumnes de Projectes!!😁\n");
+	printf("Menu\n");
+	printf("1.- Afegir repo\n");
+	do{
+		switch(opcio){
+		case 1:
+			printf("Afegir repo");
+			break;
+		default:
+			printf("No has introducido un numero valido! :\(");
+	}
+	}while (opcio != 4);
+	
+	
 
+	
 	return 0;
 }
